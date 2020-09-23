eba7251409d293d58f55415b194fbeea04af6245
Walter dos Santos <walter4asantos@gmail.com>
2020-09-23 11:46:28 -0300

    Coringa Delimitado

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,10 +15,20 @@ public class Program {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 		
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(3.0));
+		myCircles.add(new Circle(4.0));
+		
 		System.out.println("Total area: " + totalArea(myShapes));
+		System.out.println("Total area: " + totalArea(myCircles));
+		
 	}
 	
-	public static double totalArea(List<Shape> list) {
+	//Foi necessária uma alteração nos parametros do List na função totalArea, pois não funciona se for do tipo Shape para a lista myCircle
+	//Não poderia ser somente "?" porque ainda ocorre um erro de converção
+	//Então é necessário fazer como abaixo, onde funciona tanto para myShapes como para myCircle
+	//Porém não é possível adicionar qualquer item na lista
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
